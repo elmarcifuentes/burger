748505a31bec9fe2d83dd818bae76f9578dc3861
elmarcifuentes <elmarc@outlook.com>
2018-01-07 23:13:34 -0800

    completed app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Baconator Double Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Beef 'n Cheddar", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", true);
